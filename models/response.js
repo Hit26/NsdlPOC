9f6787f9b1b20c8289c8eb162a78dcb62ffcda7b
Hit26 <hiteshj2612@gmail.com>
2022-01-10 11:29:01 +0530

    updated

--- models/response.js
+++ models/response.js
@@ -2,18 +2,18 @@ const mongoose = require('mongoose');
 
 // Response Schema
 const ResponseSchema = new mongoose.Schema({
-    error:{
-        type:Number
-    },
-    code:{
-        type:Number
-    },
-    info:{
-        type:JSON
-    }
-    // requestBody:{
-    //     type:String
+    // error:{
+    //     type:Number
+    // },
+    // code:{
+    //     type:Number
+    // },
+    // info:{
+    //     type:JSON
     // }
+    requestBody:{
+        type:Object 
+    }
 });
 
 module.exports = new mongoose.model('Response',ResponseSchema);
--- routes/responses.js
+++ routes/responses.js
@@ -4,18 +4,21 @@ const Response = require('../models/response');
 //POST : CREATE A NEW RESPONSE
 
 router.post('/',(req,res)=>{
-
+    var data = req.body;
     xml = new Response({
-        error: req.body.error,
-        code : req.body.code,
-        info : req.body.info
+        // error: req.body.error,
+        // code : req.body.code,
+        // info : req.body.info
+        responseBody:data
     });
-
+    console.log(xml)
+    
     xml.save().then(xml=>{
         res.send(xml);
     }).catch(error=>{
         res.status(500).send("XML not found");
     })
+
 })
 
 module.exports = router;
